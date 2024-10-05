ea98cbbb8801d2b714cad9b13c9b63c310ac2918
micheal876 <michealedwards771@gmail.com>
2024-10-04 21:04:50 -0500

    done done

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv');
 // Load environment variables
 dotenv.config();
 
+
 // Initialize Express
 const app = express();
 
@@ -13,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, {})
    .then(() => console.log('MongoDB connected'))
    .catch(err => console.log(err));
 
